6072f728c130fe93e9eda3bd76e6c335e0c04fcf
Duclode <marvinscottk22@gmail.com>
2023-06-28 18:51:54 +0100

    got the tileset to work with the tilemap to draw the map with the corresponding tiles

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 // https://editor.p5js.org/slow_izzm/sketches/QteUQyZUu
 // a 6x10 array
 // 1 = wall 0 = space
-//tileset; 160 X 128
-//tileset tile size: 32px x 32px
+// tileset; 160 X 128
+// tileset tile size: 32px x 32px
 
+// prettier-ignore
 let tileMap = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -38,27 +39,68 @@ let tileMap = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
+
 let cellImgs = ["white", "red"];
 cellSize = 0;
 let mapWidth = tileMap[0].length;
 let mapHeight = tileMap.length;
+let tileSize = 32; // 32px x 32px
 function preload() {
-
+  tileSet = loadImage("images/pacmanTileset.png");
 }
 
+// dynamically calcualtes the dimensions of tilemap
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(150);
   ellipseMode(CORNER);
   cellSize = floor(min(width / mapWidth, height / mapHeight));
-  console.log(cellSize);
+  //console.log(cellSize);
 }
 
 function draw() {
   for (let y = 0; y < mapHeight; y++) {
     for (let x = 0; x < mapWidth; x++) {
       fill(cellImgs[tileMap[y][x]]);
-      rect(x * cellSize, y * cellSize, cellSize, cellSize)
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      image(
+        tileSet,
+        x * cellSize,
+        y * cellSize,
+        cellSize,
+        cellSize,
+        (tileMap[y][x] % 5) * tileSize,
+        floor(tileMap[y][x] / 5) * tileSize,
+        tileSize,
+        tileSize
+      );
     }
   }
+  /*
+  // when number = 0 it picks tile (0,0)
+  // when number = 5 it picks tile (0,1)
+  image(
+    tileSet,
+    windowWidth / 2.3,
+    windowHeight / 2,
+    cellSize,
+    cellSize,
+    (7 % 5) * 32, // 7 = tile number 5 = tilemap width
+    floor(7 / 5) * 32, // 7 = tile number 5 = tilema width
+    32,
+    32
+  );
+  image(
+    tileSet,
+    windowWidth / 2,
+    windowHeight / 2,
+    cellSize,
+    cellSize,
+    (0 % 5) * 32, // 0 = tile number 5 = tilemap width
+    floor(0 / 5) * 32, // 0 = tile number 5 = tilemap width
+    32,
+    32
+  );
+  */
 }
+console.log(mapWidth);
